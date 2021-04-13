2cfa4b288f305fe43dbcfee0fb7a6b3b8fa3303f
Kevin <kevinmanueldiaz@gmail.com>
2021-04-13 00:03:40 -0500

    feat: finished product recomendations
    The endpoint of recommedations was added, where the 5 most purchased
    products are recommended among people who have bought any of the
    client's products

--- api/app.py
+++ api/app.py
@@ -10,39 +10,39 @@ app = Flask(__name__)
 
 class Product(StructuredNode):
     description = StringProperty()
-    stockCode = StringProperty(unique_index=True)
+    productID = StringProperty(unique_index=True)
 
 
-class Transaction(StructuredNode):
-    transactionDate = StringProperty()
-    transactionID = StringProperty(unique_index=True)
+class Order(StructuredNode):
+    orderDate = StringProperty()
+    orderID = StringProperty(unique_index=True)
 
     product = RelationshipTo(Product, 'CONTAINS')
 
 
-class Customer(StructuredNode):
+class User(StructuredNode):
     uid = UniqueIdProperty()
-    customerID = IntegerProperty(unique_index=True)
+    userID = IntegerProperty(unique_index=True)
     country = StringProperty()
 
     # traverse outgoing IS_FROM relations, inflate to Country objects
-    transaction = RelationshipTo(Transaction, 'MADE')
+    order = RelationshipTo(Order, 'MADE')
 
 
 @app.route('/users', methods=['GET'])
 def get_users():
-    users = [{'id': user.customerID, 'country': user.country}
-             for user in Customer.nodes]
+    users = [{'id': user.userID, 'country': user.country}
+             for user in User.nodes]
     return jsonify(users)
 
 
 @app.route('/user/<id>', methods=['GET'])
 def get_user_products(id):
     products = []
-    for tran in Customer.nodes.get(customerID=id).transaction:
+    for tran in User.nodes.get(userID=id).order:
         for prod in tran.product:
-            if {'description': prod.description} not in products:
-                products.append({'description': prod.description})
+            if {'product': prod.description} not in products:
+                products.append({'product': prod.description})
     return jsonify(products)
 
 
@@ -54,17 +54,34 @@ def get_user_products(id):
 - Return recommended products but ensure that there is at least one product in Customer1 list that is not found in customer2 list (product2)
 '''
 
-"""
+
 @app.route('/recomendation/<id>', methods=['GET'])
 def get_user_recomendation(id):
-    
-    return jsonify()
+    query = '''MATCH (c1:User {userID: $_id})-[:MADE]->(:Order)-[:CONTAINS]->(p1:Product)<-[:CONTAINS]-(:Order)-[:MADE]-(c2:User)
+            WHERE c1<>c2
+            MATCH (c2)-[:MADE]->(:Order)-[:CONTAINS]->(p2:Product)
+            where p2<>p1
+            return p2.description, count(p2) as frequency
+            ORDER BY frequency DESC
+            LIMIT 5'''
 
-query = "MATCH (c1:Customer {customerID: 17850})-[:MADE]-(:Transaction)-[:CONTAINS]->(p:Product) return c1"
-results, meta = db.cypher_query(query)
-print(len(results))
-"""
+    results, meta = db.cypher_query(query, params={'_id': int(id)})
+    recomendations = [{'product': product[0]} for product in results]
+
+    # It may happen that no one has bought your product, so in that case I take the most purchased products.
+    if len(recomendations) == 0:
+        query = '''MATCH (c1:User {userID: $_id})-[:MADE]->(:Order)-[:CONTAINS]->(p1:Product)
+                MATCH (c2:User)-[:MADE]->(:Order)-[:CONTAINS]->(p2:Product)
+                where p2<>p1
+                return p2.description, count(p2) as frequency
+                ORDER BY frequency DESC
+                LIMIT 5'''
+
+        results, meta = db.cypher_query(query, params={'_id': int(id)})
+        recomendations = [{'product': product[0]} for product in results]
+
+    return jsonify(recomendations)
+
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    pass
+    app.run(debug=True)
