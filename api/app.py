447cb03fc8f5b145c6a46b2f35ca0356c16a829f
Kevin <kevinmanueldiaz@gmail.com>
2021-04-12 22:03:13 -0500

    feat: two endpoint done! test data is added
    - Test data is added to data.csv to do queries in neo4j
    - Some queries have been made and two endpoints done: /users /user/id

--- api/app.py
+++ api/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from neomodel import config
 from neomodel import (config, StructuredNode, StringProperty, IntegerProperty,
-                      UniqueIdProperty, RelationshipTo)
-
+                      UniqueIdProperty, RelationshipTo, db)
 config.DATABASE_URL = 'bolt://neo4j:1234@localhost:7687'  # default
+
+app = Flask(__name__)
 
 
 class Product(StructuredNode):
@@ -28,12 +29,41 @@ class Customer(StructuredNode):
     transaction = RelationshipTo(Transaction, 'MADE')
 
 
-# Iterable
-for c in Customer.nodes.has(transaction=True):
-    print(c.customerID)
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = [{'id': user.customerID, 'country': user.country}
+             for user in Customer.nodes]
+    return jsonify(users)
 
 
-app = Flask(__name__)
+@app.route('/user/<id>', methods=['GET'])
+def get_user_products(id):
+    products = []
+    for tran in Customer.nodes.get(customerID=id).transaction:
+        for prod in tran.product:
+            if {'description': prod.description} not in products:
+                products.append({'description': prod.description})
+    return jsonify(products)
+
+
+'''
+- First, get all products that Costumer has ordered
+- Next, find all customers that has at least one food contained in customer1's products
+- List out customer2 and collect all products for this customer2
+- Create a list of recommended products based on those found in customer1 products list BUT NOT found in customer2 produc list
+- Return recommended products but ensure that there is at least one product in Customer1 list that is not found in customer2 list (product2)
+'''
+
+"""
+@app.route('/recomendation/<id>', methods=['GET'])
+def get_user_recomendation(id):
+    
+    return jsonify()
+
+query = "MATCH (c1:Customer {customerID: 17850})-[:MADE]-(:Transaction)-[:CONTAINS]->(p:Product) return c1"
+results, meta = db.cypher_query(query)
+print(len(results))
+"""
 
 if __name__ == '__main__':
     # app.run(debug=True)
